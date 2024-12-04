94a920b95bcb828717595993f8bfbef685e01fdc
Jays-PC <jaysqvl@gmail.com>
2024-12-04 10:28:21 -0800

    updated gradle build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,6 +69,7 @@ dependencies {
     implementation(libs.play.services.auth)
     implementation(libs.kotlinx.serialization.json)
     implementation(libs.javax.inject)
+    implementation(libs.mpandroidchart)
 //    implementation("androidx.credentials:credentials:1.2.2")
 //    implementation("androidx.credentials:credentials-play-services-auth:1.2.2")
 //    implementation("com.google.android.libraries.identity.googleid:googleid:1.1.1")
